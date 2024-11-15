32c7c34c8c4eb409ceb15c90ede1f0415f1ce358
久野文菜 <ayana.kuno@mixi.co.jp>
2024-11-15 18:17:26 +0900

    refactor

---
+++ lib/example.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class Example extends StatefulWidget {
+  const Example({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<Example> createState() => _ExampleState();
+}
+
+class _ExampleState extends State<Example> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: const Center(
+        child: Text('Hello, FlutterKaigi 2024 LT!'),
+      ),
+    );
+  }
+}
