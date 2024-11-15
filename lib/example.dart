3f5b91f22becf8b847b01a374a00c4aeba248a2f
久野文菜 <ayana.kuno@mixi.co.jp>
2024-11-15 18:29:26 +0900

    use video_player

--- lib/example.dart
+++ lib/example.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
 class Example extends StatefulWidget {
   const Example({super.key, required this.title});
@@ -10,9 +11,17 @@ class Example extends StatefulWidget {
 }
 
 class _ExampleState extends State<Example> {
+  late VideoPlayerController _controller;
+
   @override
   void initState() {
     super.initState();
+    _controller = VideoPlayerController.networkUrl(Uri.parse(
+        'https://storage.googleapis.com/exoplayer-test-media-0/BigBuckBunny_320x180.mp4'))
+      ..initialize().then((_) {
+        _controller.play();
+      })
+      ..setLooping(true);
   }
 
   @override
@@ -22,8 +31,25 @@ class _ExampleState extends State<Example> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: const Center(
-        child: Text('Hello, FlutterKaigi 2024 LT!'),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Stack(
+            children: [
+              SizedBox(
+                height: 200,
+                child: VideoPlayer(_controller),
+              ),
+              Positioned(
+                left: 0,
+                bottom: 0,
+                right: 0,
+                child:
+                    VideoProgressIndicator(_controller, allowScrubbing: true),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
