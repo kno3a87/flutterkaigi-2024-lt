2a4091e5e7f16c6c0d24d8833e43f463e01df8a8
久野文菜 <ayana.kuno@mixi.co.jp>
2024-11-19 14:41:43 +0900

    scroll to top

--- lib/page1.dart
+++ lib/page1.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutterkaigi_2024_lt/scroll_wrapper.dart';
 
 class Page1 extends StatelessWidget {
   const Page1({super.key});
@@ -8,15 +9,24 @@ class Page1 extends StatelessWidget {
     // スクロールバーだしたり無限スクロールするときとかに使う
     final ScrollController scrollController = ScrollController();
 
-    return Scaffold(
-      body: Scrollbar(
-        controller: scrollController,
-        child: ListView.builder(
-          key: const PageStorageKey('page1'),
+    return ScrollWrapper(
+      onTap: () async {
+        await scrollController.animateTo(
+          0,
+          duration: const Duration(milliseconds: 300),
+          curve: Curves.easeInOut,
+        );
+      },
+      child: Scaffold(
+        body: Scrollbar(
           controller: scrollController,
-          itemCount: 100,
-          itemBuilder: (_, index) =>
-              ListTile(title: Text('Page1 ${index + 1}')),
+          child: ListView.builder(
+            key: const PageStorageKey('page1'),
+            controller: scrollController,
+            itemCount: 100,
+            itemBuilder: (_, index) =>
+                ListTile(title: Text('Page1 ${index + 1}')),
+          ),
         ),
       ),
     );
--- lib/page2.dart
+++ lib/page2.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutterkaigi_2024_lt/scroll_wrapper.dart';
 
 class Page2 extends StatefulWidget {
   const Page2({super.key});
@@ -25,12 +26,22 @@ class _Page2State extends State<Page2> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView.builder(
-        key: const PageStorageKey('page2'),
-        controller: scrollController, // 消せばステータスバータップでトップに戻れる
-        itemCount: 100,
-        itemBuilder: (_, index) => ListTile(title: Text('Page2 ${index + 1}')),
+    return ScrollWrapper(
+      onTap: () async {
+        await scrollController.animateTo(
+          0,
+          duration: const Duration(milliseconds: 300),
+          curve: Curves.easeInOut,
+        );
+      },
+      child: Scaffold(
+        body: ListView.builder(
+          key: const PageStorageKey('page2'),
+          controller: scrollController, // 消せばステータスバータップでトップに戻れる
+          itemCount: 100,
+          itemBuilder: (_, index) =>
+              ListTile(title: Text('Page2 ${index + 1}')),
+        ),
       ),
     );
   }
